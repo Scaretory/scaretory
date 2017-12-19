c922129134feddf9ed2530db698fffb97e36d5b8
Scaretory <Scaretory@gmail.com>
2017-12-19 20:01:27 +0700

    initial commit

--- www/js/routes.js
+++ www/js/routes.js
@@ -51,7 +51,7 @@ angular.module('app.routes', ['ionicUIRouter'])
       'tab1': {
         templateUrl: 'templates/ceritaSekolah.html',
         controller: 'ceritaSekolahCtrl'
-      }
+      },
     }
   })
   .state('tabsController.ceritaThriller', {
